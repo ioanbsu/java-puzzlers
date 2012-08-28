5a0f6b75d935358e9c7dc18abc061ef9c855aa3e
ioanbsu <ioanbsu1@gmail.com>
2012-08-28 16:55:55 -0700

    INTERNAL: Oddity puzzle explained.

--- src/main/java/p2_expressive_puzzlers/puzzle_1/Oddity.java
+++ src/main/java/p2_expressive_puzzlers/puzzle_1/Oddity.java
@@ -1,10 +1,24 @@
 package p2_expressive_puzzlers.puzzle_1;
 
 public class Oddity {
-    public static boolean isOdd(int i) {
-        return i % 2 == 1;
+    public static boolean wrongIsOdd(int i) {
+        return i % 2 == 1;//this does not work for negative values because of definition of [%] operation: (a/b)*b+(a%b)=a
+    }
+
+    public static boolean correctIsOdd(int i) {
+        return i % 2 !=0;
     }
 
     public static void main(String[] args) {
+        System.out.println("Wrong isOdd");
+        System.out.println(wrongIsOdd(4));
+        System.out.println(wrongIsOdd(3));
+        System.out.println(wrongIsOdd(-4));
+        System.out.println(wrongIsOdd(-3));
+        System.out.println("Correct isOdd");
+        System.out.println(correctIsOdd(4));
+        System.out.println(correctIsOdd(3));
+        System.out.println(correctIsOdd(-4));
+        System.out.println(correctIsOdd(-3));
     }
 }
