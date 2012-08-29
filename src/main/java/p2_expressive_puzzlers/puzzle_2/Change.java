ca98d7194f1ee41851fe3af537f00d23170ce667
ioanbsu <ioanbsu1@gmail.com>
2012-08-28 18:05:45 -0700

    FEARURE: long division and change examples explained.

--- src/main/java/p2_expressive_puzzlers/puzzle_2/Change.java
+++ src/main/java/p2_expressive_puzzlers/puzzle_2/Change.java
@@ -1,7 +1,10 @@
 package p2_expressive_puzzlers.puzzle_2;
 
+import java.math.BigDecimal;
+
 public class Change {
     public static void main(String args[]) {
-        System.out.println(2.00 - 1.10);
+        System.out.println(2.00 - 1.10);      //1.1 can not be represented in double correctly.
+        System.out.print(new BigDecimal("2.00").subtract(new BigDecimal("1.10") ));
     }
 }
--- src/main/java/p2_expressive_puzzlers/puzzle_3/LongDivision.java
+++ src/main/java/p2_expressive_puzzlers/puzzle_3/LongDivision.java
@@ -2,7 +2,8 @@ package p2_expressive_puzzlers.puzzle_3;
 
 public class LongDivision {
     public static void main(String[] args) {
-        final long MICROS_PER_DAY = 24 * 60 * 60 * 1000 * 1000;
+//        final long MICROS_PER_DAY = 24 * 60 * 60 * 1000 * 1000; //incorrect way
+        final long MICROS_PER_DAY = 24L * 60 * 60 * 1000 * 1000; //correct way
         final long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
         System.out.println(MICROS_PER_DAY / MILLIS_PER_DAY);
     }
